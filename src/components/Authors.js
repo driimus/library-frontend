a6dac93b45549b3281a5827a69076dc271621a27
Cristi Petre <driimus@vivaldi.net>
2020-05-24 14:53:07 +0100

    Wire in allAuthors query

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,11 +1,19 @@
-  
+
 import React from 'react'
+import { useQuery } from '@apollo/client'
+
+import { ALL_AUTHORS } from '../queries'
 
 const Authors = (props) => {
+  const authors = useQuery(ALL_AUTHORS)
+
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (authors.loading) {
+    return (<div>loading ...</div>)
+  }
 
   return (
     <div>
@@ -21,7 +29,7 @@ const Authors = (props) => {
               books
             </th>
           </tr>
-          {authors.map(a =>
+          {authors.data.allAuthors.map(a =>
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
